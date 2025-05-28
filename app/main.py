4a9d547419b71ac5b55ba7ca27bb25b35ddaff0e
Yevhen Nastoiashchyi <Nastoyashiy@i.ua>
2025-05-28 22:50:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+
         Person.people[name] = self
 
 
